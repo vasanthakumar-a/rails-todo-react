ae550a0fc3f71efe1ce30cd88e5c237e630cbbf8
Vasanthakumar <vasanthakumara117@gmail.com>
2023-01-19 17:53:46 +0530

    migrated completed

--- db/migrate/20230118172636_create_todo_lists.rb
+++ db/migrate/20230118172636_create_todo_lists.rb
@@ -2,7 +2,7 @@ class CreateTodoLists < ActiveRecord::Migration[7.0]
   def change
     create_table :todo_lists do |t|
       t.string :todo_item
-      t.string :status
+      t.boolean :completed
 
       t.timestamps
     end
