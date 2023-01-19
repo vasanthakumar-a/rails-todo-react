6ef235935cb1a9bdd27869791054bf93574a334a
Vasanthakumar <vasanthakumara117@gmail.com>
2023-01-19 17:53:57 +0530

    seeded some values

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts('Creating Todos')
+
+@todos = [{ todo_item: 'Vasanth', completed: true }, { todo_item: 'Tushar', completed: false },
+          { todo_item: 'Mithun', completed: false }, { todo_item: 'Sowndar', completed: true },
+          { todo_item: 'Srinandhini', completed: false }, { todo_item: 'Aboorva', completed: true }]
+TodoList.create!(@todos)
+
+puts('Todos Created Successfully')
